caf38b80db89562e74c8da3535db7753c4f203d4
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2025-02-20 19:33:21 +0600

    remove commets

--- strings/name_begin_with_a.py
+++ strings/name_begin_with_a.py
@@ -1,7 +1,4 @@
 # Write a Python Program to get Name of User that starts with ‘a’, otherwise Error Message Should Display
-# Write a Python Program to get Name of User that Ends with ‘n’, otherwise Error Message Should Display
-# Write a Python Program to get a Sentence from User, And Display Character of that Sentence that Present on Odd Index Number
-
 
 name = input("Enter a name: ")
 name = name.lower()
