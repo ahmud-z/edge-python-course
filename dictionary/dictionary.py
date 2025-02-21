7799c39b0017c881ee640823851cd658fa5b0155
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2025-02-21 10:31:34 +0600

    example of python dictionary

---
+++ dictionary/dictionary.py
@@ -0,0 +1,6 @@
+students = {"student1": {"name": "ahmud", "age": 23, "dept": "CSE"}}
+
+print("\nDetails of first Student: \n")
+print(f"Name: {students["student1"]["name"].upper()}")
+print(f"Age: {students["student1"]["age"]}")
+print(f"Department: {students["student1"]["dept"].upper()}")
