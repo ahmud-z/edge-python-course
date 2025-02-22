ec9b7957b7266930705de92e93f3db89f653c5e9
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2025-02-22 21:13:18 +0600

    deleted

--- dictionary/list_to_dict.py
+++
@@ -1,8 +0,0 @@
-# Write a Python Program to Create a Dictionary from List which multiplies the values with 2 using Dictionary comprehension.
-
-numbers = [1, 2, 3, 4, 5]
-
-new_dict = {key: (key * 2) for (key) in numbers}
-
-for key, value in new_dict.items():
-    print(f"{key} : {value}")
