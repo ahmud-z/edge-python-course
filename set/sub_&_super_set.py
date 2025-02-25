dabee4c4f7f2c4fbcb5cfdebb6eb6dc583523d37
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2025-02-25 21:52:16 +0600

    set union, intersection

---
+++ set/sub_&_super_set.py
@@ -0,0 +1,6 @@
+set1 = {1, 2, 3, 5, 4}
+set2 = {1, 3, 2}
+
+print(set2.issubset(set1))
+
+print(set2.issuperset(set1))
---
+++ set/union_intersection.py
@@ -0,0 +1,13 @@
+set1 = {1, 4, 5, 4, 1, 8, 1, 5, 3, 8}
+set2 = {3, 2, 5, 6, 3, 4, 3, 7, 4, 1}
+
+print(f"set1 = {set1}")
+print(f"set2 = {set2}")
+
+set_union = set1.union(set2)
+print("\nResult of Set Union: ")
+print(set_union)
+
+set_intersection = set1.intersection(set2)
+print("\nResult of Set Intersection: ")
+print(set_intersection)
